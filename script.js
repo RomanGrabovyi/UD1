080d518086db1ca68bab353804355ba9f19d022a
Roman Grabovyi <romansudova@gmail.com>
2020-05-18 11:56:35 +0300

    mod lesson add loops

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const numberOfFilm = +prompt('How many films are you watched?', '');
+const numberOfFilm = prompt('How many films did you watched?', '');
 
 const personalmovieDB = {
     count: numberOfFilm,
@@ -8,12 +8,29 @@ const personalmovieDB = {
     privat: false
 };
 
-const a = prompt('What is the last film you watch?',''),
-      b = prompt('How did you rate this film?',''),
-      c = prompt('What is the last film you watch?',''),
-      d = prompt('How did you rate this film?','');
 
-personalmovieDB.movies[a] = b;
-personalmovieDB.movies[c] = d;
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('What is the last film you watch?',''),
+          b = prompt('How did you rate this film?','');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+    
+    if (personalMovieDB.count < 10) {
+        console.log("Просмотрено довольно мало фильмов");
+    } else if ( personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("Вы классический зритель");
+    } else if (personalMovieDB.count >= 30) {
+        console.log("Вы киноман");
+    } else {
+        console.log("Произошла ошибка");
+    }
+}
 
 console.log(personalmovieDB);
